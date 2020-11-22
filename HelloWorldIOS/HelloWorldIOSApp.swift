07a5a7a703ff763c25c5d5b690d2b292751b2139
lauwarm <10065855+lauwarm@users.noreply.github.com>
2020-11-22 21:38:47 +0100

    Initial Commit

---
+++ HelloWorldIOS/HelloWorldIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldIOSApp.swift
+//  HelloWorldIOS
+//
+//  Created by Fabian Bader on 22.11.20.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
